a75c0222e5997b70825158d50825ff1a62b2d6b0
jheroje <javierho93@gmail.com>
2025-12-07 02:51:11 +0000

    add resizable columns

--- src/renderer/src/components/CenterBottomPanel.tsx
+++ src/renderer/src/components/CenterBottomPanel.tsx
@@ -1,3 +1,4 @@
+import { QueryRow } from '@common/types';
 import { useTabs } from '@renderer/contexts/tabs/TabsContext';
 import { ScrollArea, ScrollBar } from '@renderer/shadcn/ui/scroll-area';
 import {
@@ -8,11 +9,45 @@ import {
   TableHeader,
   TableRow,
 } from '@renderer/shadcn/ui/table';
-import React from 'react';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import React, { useMemo } from 'react';
 
 export function CenterBottomPanel(): React.JSX.Element {
   const { activeTab } = useTabs();
   const { queryError, queryResult } = activeTab;
+
+  const data = useMemo(() => queryResult?.rows || [], [queryResult?.rows]);
+
+  const columns = useMemo(() => {
+    if (!queryResult?.columns) return [];
+
+    const columnHelper = createColumnHelper<QueryRow>();
+
+    return queryResult.columns.map((col, index) =>
+      columnHelper.accessor((row) => row.values[index], {
+        id: col.__columnId,
+        header: col.name,
+        cell: (info) =>
+          typeof info.getValue() === 'object'
+            ? JSON.stringify(info.getValue())
+            : String(info.getValue()),
+        enableResizing: true,
+      })
+    );
+  }, [queryResult?.columns]);
+
+  // eslint-disable-next-line react-hooks/incompatible-library
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode: 'onChange',
+  });
 
   return (
     <>
@@ -29,28 +64,40 @@ export function CenterBottomPanel(): React.JSX.Element {
             <div className="w-max min-w-full">
               <Table>
                 <TableHeader className="bg-muted sticky top-0 z-10">
-                  <TableRow>
-                    {queryResult.columns.map(({ __columnId, name }) => (
-                      <TableHead
-                        key={__columnId}
-                        className="font-medium text-xs text-muted-foreground"
-                      >
-                        {name}
-                      </TableHead>
-                    ))}
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {queryResult.rows.map(({ __rowId, values }) => (
-                    <TableRow key={__rowId}>
-                      {queryResult.columns.map(({ __columnId }, i) => (
-                        <TableCell
-                          key={__columnId}
-                          className="border-r last:border-r-0 max-w-[200px] truncate text-xs"
+                  {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => (
+                        <TableHead
+                          key={header.id}
+                          style={{ width: header.getSize() }}
+                          className="font-medium text-xs text-muted-foreground relative"
                         >
-                          {typeof values[i] === 'object'
-                            ? JSON.stringify(values[i])
-                            : String(values[i])}
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(header.column.columnDef.header, header.getContext())}
+
+                          {header.column.getCanResize() && (
+                            <div
+                              onMouseDown={header.getResizeHandler()}
+                              className="absolute -right-1 top-0 h-full w-2 cursor-col-resize select-none z-100"
+                            />
+                          )}
+                        </TableHead>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableHeader>
+
+                <TableBody>
+                  {table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id}>
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell
+                          key={cell.id}
+                          style={{ width: cell.column.getSize() }}
+                          className="border-r last:border-r-0 text-xs"
+                        >
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
                         </TableCell>
                       ))}
                     </TableRow>
